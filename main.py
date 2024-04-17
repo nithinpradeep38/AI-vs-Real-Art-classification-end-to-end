4eb7b516c57f73d6818e8f5e7e92c08d99b53e69
nithin pradeep <nithinpradeep@nithins-MacBook-Pro.local>
2024-04-16 21:05:57 -0700

    Model prepared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from AI_Real_Classifier import logging
 from AI_Real_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from AI_Real_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME= "Data Ingestion stage"
 
@@ -8,6 +9,17 @@ try:
     ingest= DataIngestionTrainingPipeline()
     ingest.main()
     logging.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logging.exception(e)
+    raise e
+
+STAGE_NAME= "Prepare Model"
+try:
+    logging.info(f"*******************")
+    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_model = PrepareBaseModelTrainingPipeline()
+    prepare_model.main()
+    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logging.exception(e)
     raise e
